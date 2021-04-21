cbd5e4b33ccd339522a6ba0b833507c9525a4b0e
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-04-21 11:38:41 -0300

    feature: adicionado parâmetros na chamada da api e tipagem da página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,17 @@
-export default function Home(props) {
+import { GetStaticProps } from "next";
+
+type Episode = {
+  id: string;
+  title: string;
+  member: string;
+};
+
+type HomeProps = {
+  // quando declaramos um array, ele pode ser de vários tipos, neste caso estamos definindo que ele é do tipo Episodes.
+  episodes: Episode[];
+};
+
+export default function Home(props: HomeProps) {
   return (
     <>
       <h1>Index</h1>
@@ -7,9 +20,11 @@ export default function Home(props) {
   );
 }
 
-export async function getStaticProps() {
-  const response = await fetch("http://localhost:3333/episodes");
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch(
+    "http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc"
+  ); // adicionando alguns parâmetros direto na chamada da API para filtrar os resultados.
+  const data = await response.json(); // dados retornados da API
 
   return {
     props: {
@@ -17,4 +32,4 @@ export async function getStaticProps() {
     },
     revalidate: 60 * 60 * 8, // define o intervalo em qua a aplicação fará uma nova chamada para a api e atualizará a pagina de 8 em 8 horas.
   };
-}
+};
