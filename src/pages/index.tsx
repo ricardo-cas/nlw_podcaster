d7b01b8e7f425148dfb3f24c81c6b4721ea91a92
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-04-21 01:57:11 -0300

    feature: consumindo dados de API com JSON e executado primeiro build da aplicação sendo uma aplicação estática

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+export default function Home(props) {
   return (
     <>
       <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, // define o intervalo em qua a aplicação fará uma nova chamada para a api e atualizará a pagina de 8 em 8 horas.
+  };
 }
