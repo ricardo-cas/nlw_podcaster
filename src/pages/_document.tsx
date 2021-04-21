c1e709e733c5ee431486d1bda085b70ac1556887
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-04-20 23:35:46 -0300

    feat: configurando estilos globais da aplicação

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main /> {/* onde vai ficar a aplicação */}
+          <NextScript />{" "}
+          {/* onde vai ficar os scripts que o next precisa para que o nosso app funcione */}
+        </body>
+      </Html>
+    );
+  }
+}
