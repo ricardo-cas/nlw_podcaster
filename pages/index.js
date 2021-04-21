b818474850c542e4919ba265b6deaa2be0146cb2
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-04-20 21:50:07 -0300

    fix: index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
     <>
-    <h1>Hello World in Next.js</h1>
-    <p>Have fun!</p>
+      <h1>Hello World in Next.js</h1>
+      <p>Have fun!</p>
     </>
-  )
+  );
 }
