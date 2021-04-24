420ca56de496c484576ba2416e067a8e0fd3d27f
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-04-24 14:21:35 -0300

    update: adicionado link no logo do site para direcionar para a homepage

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,6 @@
 import format from "date-fns/format";
 import ptBR from "date-fns/locale/pt-BR";
+import Link from "next/link";
 
 import styles from "./styles.module.scss";
 
@@ -9,7 +10,11 @@ export function Header() {
   });
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Logo do Podcaster" />
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="Logo do Podcaster" />
+        </a>
+      </Link>
 
       <p>O melhor para você ouvir, sempre</p>
       <span>{currentDate}</span>
