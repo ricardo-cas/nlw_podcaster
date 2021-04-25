eae4ff6a671ef4a2c2deb4a85313ba3cba24d71a
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-04-25 17:58:16 -0300

    update: refatorando código.

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,63 @@
+import { createContext, ReactNode, useState } from "react";
+
+type Episode = {
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: number;
+  url: string;
+};
+
+// criando a tipagem das informações que irei salvar dentro do contexto
+type PlayerContextData = {
+  episodeList: Episode[];
+  currentEpisodeIndex: number; // vai ser o índice que aponta em qual posição da lista é o episodio que está tocando atualmente.
+  isPlaying: boolean;
+  play: (episode: Episode) => void;
+  togglePlay: () => void;
+  setPlayingState: (state: boolean) => void;
+};
+
+// exportando o contexto com um valor default
+export const PlayerContext = createContext({} as PlayerContextData);
+
+type PlayerContextProviderProps = {
+  children: ReactNode;
+};
+
+export function PlayerContextProvider({
+  children,
+}: PlayerContextProviderProps) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode: Episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  //função pause
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+  return (
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        play,
+        isPlaying,
+        togglePlay,
+        setPlayingState,
+      }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  );
+}
