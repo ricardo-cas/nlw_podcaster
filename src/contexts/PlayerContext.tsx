e04195aceea262bdc6ace56c2658b65239e88520
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-04-25 22:18:13 -0300

    feature: criado botão shuffle para habilitar o modo aleatório

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -14,10 +14,12 @@ type PlayerContextData = {
   currentEpisodeIndex: number; // vai ser o índice que aponta em qual posição da lista é o episodio que está tocando atualmente.
   isPlaying: boolean;
   isLooping: boolean;
+  isShuffling: boolean;
   play: (episode: Episode) => void;
   playList: (list: Episode[], index: number) => void;
   togglePlay: () => void;
   toggleLoop: () => void;
+  toggleShuffle: () => void;
   setPlayingState: (state: boolean) => void;
   playNext: () => void;
   playPrevious: () => void;
@@ -39,6 +41,7 @@ export function PlayerContextProvider({
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLooping, setIsLooping] = useState(false);
+  const [isShuffling, setIsShuffling] = useState(false);
 
   function playList(list: Episode[], index: number) {
     setEpisodeList(list);
@@ -56,17 +59,30 @@ export function PlayerContextProvider({
   function togglePlay() {
     setIsPlaying(!isPlaying);
   }
+
   function toggleLoop() {
     setIsLooping(!isLooping);
   }
+
+  function toggleShuffle() {
+    setIsShuffling(!isShuffling);
+  }
+
   function setPlayingState(state: boolean) {
     setIsPlaying(state);
   }
+
   const hasPrevious = currentEpisodeIndex > 0;
   const hasNext = currentEpisodeIndex + 1 < episodeList.length;
 
   function playNext() {
-    if (hasNext) {
+    if (isShuffling) {
+      const nextRandomEpisodeIndex = Math.floor(
+        Math.random() * episodeList.length
+      );
+
+      setCurrentEpisodeIndex(nextRandomEpisodeIndex);
+    } else if (hasNext) {
       setCurrentEpisodeIndex(currentEpisodeIndex + 1);
     }
   }
@@ -88,8 +104,10 @@ export function PlayerContextProvider({
         playPrevious,
         isPlaying,
         isLooping,
+        isShuffling,
         toggleLoop,
         togglePlay,
+        toggleShuffle,
         setPlayingState,
         hasNext,
         hasPrevious,
